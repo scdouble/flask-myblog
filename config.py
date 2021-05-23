2f39578de4d004789de51d56f1082953ee1cd484
Tatsu Sei <doubledragontaz@gmail.com>
2021-05-23 22:42:46 +0900

    Creating models with SQLAlchemy

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 class Config(object):
-    pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:password@localhost:15432/myblog"
